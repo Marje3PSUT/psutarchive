4482b6db99e71aaf1316aa1fc25cd709705f1db8
Mohamed Mansour <mohmans@pm.me>
2023-04-26 16:48:11 +0300

    Update navigation bar

--- components/Home/Nav.vue
+++ components/Home/Nav.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="navbar-start">
+      <!-- Navigation menu on sm/md screens -->
       <div class="dropdown">
         <label tabindex="0" class="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </label>
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a>Item 1</a></li>
+        <ul
+          tabindex="0"
+          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-60 border"
+        >
           <li tabindex="0">
-            <a class="justify-between">
-              Parent
-              <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-            </a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
+            <NuxtLink to="/courses">
+              Courses
+            </NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/upload">
+              Upload
+            </NuxtLink>
           </li>
           <li>
             <NuxtLink to="/about">
@@ -31,41 +34,42 @@
         PSUT Archive
       </NuxtLink>
     </div>
+    <!-- Navigation menu on large screens -->
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
         <li tabindex="0">
-          <a>
-            Parent
-            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-          </a>
-          <ul class="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <NuxtLink to="/courses">
+            Courses
+          </NuxtLink>
         </li>
         <li>
-            <NuxtLink to="/about">
-              About
-            </NuxtLink>
-          </li>
+          <NuxtLink to="/upload">
+            Upload
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">
+            About
+          </NuxtLink>
+        </li>
       </ul>
     </div>
     <div class="navbar-end">
-      <button
-        class="btn btn-circle mx-4"
-        @click="toggleTheme"
-      >
-        theme
-      </button>
-      <a class="btn">Get started</a>
+      <!-- Theme toggle -->
+      <label class="swap swap-rotate">
+        <input type="checkbox" class="mx-4" @change="toggleTheme"/> 
+        <svg
+          class="swap-on"
+          xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9.37 5.51A7.35 7.35 0 0 0 9.1 7.5c0 4.08 3.32 7.4 7.4 7.4c.68 0 1.35-.09 1.99-.27A7.014 7.014 0 0 1 12 19c-3.86 0-7-3.14-7-7c0-2.93 1.81-5.45 4.37-6.49zM12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26a5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1z"/></svg>
+        <svg
+          class="swap-off"
+          xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 9c1.65 0 3 1.35 3 3s-1.35 3-3 3s-3-1.35-3-3s1.35-3 3-3m0-2c-2.76 0-5 2.24-5 5s2.24 5 5 5s5-2.24 5-5s-2.24-5-5-5zM2 13h2c.55 0 1-.45 1-1s-.45-1-1-1H2c-.55 0-1 .45-1 1s.45 1 1 1zm18 0h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1zM11 2v2c0 .55.45 1 1 1s1-.45 1-1V2c0-.55-.45-1-1-1s-1 .45-1 1zm0 18v2c0 .55.45 1 1 1s1-.45 1-1v-2c0-.55-.45-1-1-1s-1 .45-1 1zM5.99 4.58a.996.996 0 0 0-1.41 0a.996.996 0 0 0 0 1.41l1.06 1.06c.39.39 1.03.39 1.41 0s.39-1.03 0-1.41L5.99 4.58zm12.37 12.37a.996.996 0 0 0-1.41 0a.996.996 0 0 0 0 1.41l1.06 1.06c.39.39 1.03.39 1.41 0a.996.996 0 0 0 0-1.41l-1.06-1.06zm1.06-10.96a.996.996 0 0 0 0-1.41a.996.996 0 0 0-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06zM7.05 18.36a.996.996 0 0 0 0-1.41a.996.996 0 0 0-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0l1.06-1.06z"/></svg>
+      </label>
     </div>
   </div>
 </template>
 <script setup>
-
 const toggleTheme = () => {
   useTheme().value = useTheme().value == 'light' ? 'dark' : 'light';
 }
-
 </script>
