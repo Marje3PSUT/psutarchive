6687d202d420d408cb02fcfd9d5beb9e9328f337
Mohamed Mansour <mohmans@pm.me>
2023-05-18 17:21:06 +0300

    Add courses list demo

--- pages/courses/index.vue
+++ pages/courses/index.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>courses list</div>
+  <div class="container mx-auto">
+    <div
+      v-if="state.courses"
+      class="flex flex-wrap"
+    >
+      <div
+        v-for="item, i in state.courses.data"
+        :key="i"
+        class="card w-96 bg-base-100 shadow-xl"
+      >
+        <div class="card-body">
+          <h2 class="card-title">
+            {{ item.attributes.Name }}
+          </h2>
+          <p>{{ item.attributes.School }}</p>
+          <p>{{ item.attributes.CourseID }}</p>
+          <div class="card-actions justify-start mt-4">
+            <button class="btn btn-primary">
+              See Exams
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+const state = reactive({
+  courses: []
+})
+const { find } = useStrapi()
+console.log(useStrapiUrl()) // FIXME: Strapi can only read the URL in .env on the client side
+onMounted(async() => {
+  const courses = await find('courses');
+  state.courses = courses;
+})
+</script>
