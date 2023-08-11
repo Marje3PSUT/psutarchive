b087888567fd81f1419121da837250c28462c9d2
Mohamed Mansour <mohmans@pm.me>
2023-08-11 20:34:11 +0300

    feat(homepage): add categories list

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <SearchBar />
+  <div class="container mx-auto">
+    <!-- <ListSearch />
+    <HomeCategoriesList /> -->
+    <List
+      :pending="pending"
+      view="flex"
+      :heading="$t('home.categories.title')"
+      class="mx-auto flex-row flex-wrap gap-4"
+      :heading-link="{
+        to: localePath('/courses'),
+        text: $t('home.categories.all'),
+        icon: true
+      }"
+    >
+      <NuxtLink
+        v-for="cat in categories?.data"
+        :key="cat.id"
+        class="card max-h-24 shadow-xl border max-md:w-40"
+        :class="`hover:bg-${cat.attributes.abbreviation} border-${cat.attributes.abbreviation}`"
+        :to="`${localePath('/courses')}?cat=${cat.attributes.abbreviation.toLowerCase()}`"
+      >
+        <div class="card-body p-4 font-bold justify-center">
+          {{ locale === "en" ? cat.attributes.name : cat.attributes.name_ar }}
+        </div>
+      </NuxtLink>
+    </List>
   </div>
 </template>
+<script setup lang="ts">
+  const { locale } = useI18n();
+  const { data: categories, pending } = useLazyAsyncData<
+    StrapiResponse<CategoryAttributes>
+  >(() => $baseApi("categories"));
+</script>
