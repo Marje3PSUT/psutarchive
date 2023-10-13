6e26e006fc069283d75901157d8712dff25a068d
Mohamed Mansour <mohmans@pm.me>
2023-10-13 18:57:43 +0300

    fix(about-page): styling

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
   <div
-    class="container mx-auto"
+    class="about-page container mx-auto"
     v-html="
       locale === 'en'
         ? about?.data.attributes.page_en
@@ -16,3 +16,31 @@
     return $baseApi("about-page");
   });
 </script>
+
+<style lang="postcss">
+.about-page {
+  h1, h2, h3 {
+    @apply my-4;
+  }
+
+  h2 {
+    @apply my-8;
+  }
+  
+  li {
+    @apply list-item my-2;
+  }
+  
+  ul {
+    @apply list-disc list-inside my-5;
+  }
+  
+  strong {
+    @apply font-extrabold;
+  }
+  
+  a {
+    @apply link link-hover text-secondary;
+  }
+}
+</style>
