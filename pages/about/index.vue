fec0a03370c96c9b7abe6bcbee9e74dbff3e4408
Mohamed Mansour <mohmans@pm.me>
2023-10-13 16:28:22 +0300

    refactor: about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,24 +1,16 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
-  <div class="container mx-auto">
-      <div v-html="locale === 'en' ? aboutPageData.page_en : aboutPageData.page_ar"></div>
-  </div>
+  <div
+    class="container mx-auto"
+    v-html="locale === 'en' ? about?.data.attributes.page_en : about?.data.attributes.page_ar"
+  />
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { useI18n } from 'vue-i18n';
-
 const { locale } = useI18n();
 
-const aboutPageData = ref<{ page_en: string, page_ar: string }>({ page_en: '', page_ar: '' });
-
-const fetchData = async () => {
-  const response = await $baseApi('about-page');
-  if (response && response.data) {
-    aboutPageData.value = response.data.attributes;
-  }
-};
-
-// Call when mounted
-onMounted(fetchData);
+const { data: about } =
+  await useAsyncData<StrapiResponseSingle<AboutAttributes>>(async () => {
+  return $baseApi('about-page');
+});
 </script>
