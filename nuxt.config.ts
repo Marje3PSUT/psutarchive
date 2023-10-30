dc23b618bcbfedfca6ce201638e8f1bb38dd68bc
Mohammad Khawaja <104163142+AL-Khawaja@users.noreply.github.com>
2023-10-30 11:02:37 +0300

    SEO optimizations
    complete nuxt.config.ts file tailored for the PSUT Archive project by Marje3 Student Club, including SEO optimizations and the content.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+head: {
+    title: 'PSUT Archive - Marje3 Student Club',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'PSUT Archive is a comprehensive web archive for exams, past papers, and student notes, created by Marje3 Student Club. Find all the academic resources you need for PSUT courses.' },
+      { name: 'keywords', content: 'PSUT, Archive, Marje3, Student Club, Exams, Past Papers, Notes, Study Materials, Academic Resources' },
+      { property: 'og:title', content: 'PSUT Archive - Marje3 Student Club' },
+      { property: 'og:description', content: 'A comprehensive web archive for exams, past papers, and student notes at PSUT. Created by Marje3 Student Club to support academic excellence.' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://psutarchive.com' },
+      { property: 'og:image', content: 'https://psutarchive.com/Logo.svg' },
+      { property: 'twitter:card', content: 'summary_large_image' },
+    ],
   devtools: { enabled: true },
   modules: ["@nuxtjs/i18n", "nuxt-icon", "nuxt-api-party"],
   css: ["~/assets/css/main.css", "~/assets/css/typography.css"],
