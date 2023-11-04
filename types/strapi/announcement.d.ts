34b898c5b07d596a924c62d4ad46781a4610e850
Mohamed Mansour <mohmans@pm.me>
2023-11-04 15:59:49 +0300

    feat: add announcement componenet

---
+++ types/strapi/announcement.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  interface AnnouncementAttributes {
+    title: string;
+    title_ar: string;
+    description: string | null;
+    description_ar: string | null;
+    link: string | null;
+  }
+}
+export {};
