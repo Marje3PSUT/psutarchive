0afa3017ce82781067a05a8e8e6af59bfbc24360
HDDTHR <stevenjildeh@gmail.com>
2023-11-11 10:49:42 +0300

    enhance: remove resource type from card

--- components/Resource/Card.vue
+++ components/Resource/Card.vue
@@ -1,41 +1,89 @@
 <template>
   <label class="h-28 block resource-card shadow-xl px-3">
-    <input type="checkbox" name="resources" class="hidden absolute" @click="checkboxLogic">
+    <input
+      type="checkbox"
+      name="resources"
+      class="hidden absolute"
+      @click="checkboxLogic"
+    >
     <div class="relative border border-neutral-content rounded-xl h-full select-none">
       <!-- Solved indicator -->
-      <div v-if="item.material[0]?.is_solved" :class="indicator">
+      <div
+        v-if="item.material[0]?.is_solved"
+        :class="indicator"
+      >
         {{ $t("material.resource.exam.solved") }}
       </div>
       <!-- Card -->
       <div class="relative w-full h-full overflow-hidden">
         <!-- Drawer Div -->
         <div
-          class="top-1/2 absolute w-24 h-full -translate-y-1/2 bg-secondary text-secondary-content border-l border-l-neutral-content rounded-xl resource-drawer z-10">
+          class="top-1/2 absolute w-24 h-full -translate-y-1/2 bg-secondary text-secondary-content border-l border-l-neutral-content rounded-xl resource-drawer z-10"
+        >
           <div class="grid grid-cols-2 h-4/5">
-            <button aria-label="report-button" :title="$t('material.report')"
-              class="hover:text-neutral-50 transition-colors">
-              <Icon name="ion:flag-outline" size="24" />
+            <button
+              aria-label="report-button"
+              :title="$t('material.report')"
+              class="hover:text-neutral-50 transition-colors"
+            >
+              <Icon
+                name="ion:flag-outline"
+                size="24"
+              />
             </button>
 
-            <button aria-label="download-button" :title="$t('material.share')"
-              class="hover:text-neutral-50 transition-colors">
-              <Icon name="ion:share-social-outline" size="24" />
+            <button
+              aria-label="download-button"
+              :title="$t('material.share')"
+              class="hover:text-neutral-50 transition-colors"
+            >
+              <Icon
+                name="ion:share-social-outline"
+                size="24"
+              />
             </button>
-            <a v-if="!containsMultipleFiles" :href="item.files?.data[0].attributes.url" :title="$t('material.open')"
-              :download="item.files?.data[0].attributes.name" target="_blank"
-              class="flex justify-center items-center hover:text-neutral-50 transition-colors">
-              <Icon name="ion:md-open" size="24" />
+            <a
+              v-if="!containsMultipleFiles"
+              :href="item.files?.data[0].attributes.url"
+              :title="$t('material.open')"
+              :download="item.files?.data[0].attributes.name"
+              target="_blank"
+              class="flex justify-center items-center hover:text-neutral-50 transition-colors"
+            >
+              <Icon
+                name="ion:md-open"
+                size="24"
+              />
             </a>
-            <button v-else aria-label="see-files-button" :title="$t('material.preview')"
-              class="hover:text-neutral-50 transition-colors" @click="openModal()">
-              <Icon name="ion:list-outline" size="24" />
+            <button
+              v-else
+              aria-label="see-files-button"
+              :title="$t('material.preview')"
+              class="hover:text-neutral-50 transition-colors"
+              @click="openModal()"
+            >
+              <Icon
+                name="ion:list-outline"
+                size="24"
+              />
             </button>
-            <button v-if="!loading" aria-label="download-button" :title="$t('material.download')"
-              class="hover:text-neutral-50 transition-colors" @click="download(item.files?.data)">
-              <Icon name="ion:md-download" size="24" />
+            <button
+              v-if="!loading"
+              aria-label="download-button"
+              :title="$t('material.download')"
+              class="hover:text-neutral-50 transition-colors"
+              @click="download(item.files?.data)"
+            >
+              <Icon
+                name="ion:md-download"
+                size="24"
+              />
             </button>
-            <div v-else class="flex justify-center items-center">
-              <span class="res-loading loading loading-spinner w-5"></span>
+            <div
+              v-else
+              class="flex justify-center items-center"
+            >
+              <span class="res-loading loading loading-spinner w-5" />
             </div>
           </div>
           <div class="text-xs opacity-75 text-center h-1/5">
@@ -47,30 +95,34 @@
         <!-- Content Div-->
         <div class="ml-6 w-3/4 h-full pt-1">
           <div :class="'text-lg font-bold mb-1 ' + titleSize">
-            {{ $t(`material.resource.type.${resourceType}`) }} -
-            <span class="text-secondary">{{
-              item?.material[0]?.type &&
-              $t(
-                `material.resource.${resourceType}.type.${item.material[0].type}`
-              )
-            }}</span>
+            <span class="text-secondary">
+              {{
+                item?.material[0]?.type &&
+                  $t(
+                    `material.resource.${resourceType}.type.${item.material[0].type}`
+                  )
+              }}
+            </span>
           </div>
           <div :class="'text-md font-bold ' + titleSize">
             {{
               item?.metadata?.semester &&
-              $t(`material.semesters.${item.metadata.semester.toLowerCase()}`)
+                $t(`material.semesters.${item.metadata.semester.toLowerCase()}`)
             }}
             <span class="text-secondary">{{ item?.metadata?.year }}</span>
           </div>
-          <div v-if="resourceType === 'note'" class="text-xs opacity-75 line-clamp-2 h-2/5">
+          <div
+            v-if="resourceType === 'note'"
+            class="text-xs opacity-75 line-clamp-2 h-2/5"
+          >
             {{ item.material[0]?.title }}
           </div>
           <div class="font-mono text-xs opacity-50 h-1/5">
             {{
               item.createdBy
-              ? `${$t("misc.by")} ${item.createdBy.firstname} ${item.createdBy.lastname
-              }`
-              : `${$t("misc.by")} ${$t("misc.team")}`
+                ? `${$t("misc.by")} ${item.createdBy.firstname} ${item.createdBy.lastname
+                }`
+                : `${$t("misc.by")} ${$t("misc.team")}`
             }}
           </div>
         </div>
@@ -79,8 +131,15 @@
   </label>
 
   <!-- preview files modal -->
-  <ResourceModal v-if="filesCount > 1" :id="resourceId" @closed="modalIsOpen = false">
-    <template v-if="modalIsOpen" #content>
+  <ResourceModal
+    v-if="filesCount > 1"
+    :id="resourceId"
+    @closed="modalIsOpen = false"
+  >
+    <template
+      v-if="modalIsOpen"
+      #content
+    >
       <div class="border-b border-base-content border-opacity-50 my-4">
         <div class="capitalize text-lg font-bold flex justify-between p-2">
           <span>
@@ -105,24 +164,49 @@
             {{ item.metadata?.year }}
           </span>
         </div>
-        <div v-if="item.material[0]?.title" class="text-xs p-2">
+        <div
+          v-if="item.material[0]?.title"
+          class="text-xs p-2"
+        >
           {{ item.material[0]?.title }}
         </div>
       </div>
       <div class="flex flex-col gap-2">
-        <div v-for="file in item.files?.data" :key="file.id" dir="ltr"
-          class="border-b border-base-content border-opacity-25 p-2 flex items-center gap-4 last:border-none">
+        <div
+          v-for="file in item.files?.data"
+          :key="file.id"
+          dir="ltr"
+          class="border-b border-base-content border-opacity-25 p-2 flex items-center gap-4 last:border-none"
+        >
           <span class="font-mono">{{ file.attributes.name }}</span>
           <span class="text-center text-sm ms-auto">
             {{ (file.attributes.size / 1024).toFixed(2) }}
             {{ $t("material.megabyte") }}
           </span>
-          <a :href="file.attributes.url" target="_blank" :download="file.attributes.name" :title="$t('material.open')">
-            <Icon name="ion:md-open" size="24" class="cursor-pointer hover:text-accent transition-[200ms]" />
+          <a
+            :href="file.attributes.url"
+            target="_blank"
+            :download="file.attributes.name"
+            :title="$t('material.open')"
+          >
+            <Icon
+              name="ion:md-open"
+              size="24"
+              class="cursor-pointer hover:text-accent transition-[200ms]"
+            />
           </a>
-          <button v-if="!loading" aria-label="download-button" class="res-download tooltip tooltip-accent z-20"
-            :data-tip="$t('material.download', filesCount)" @click="download(item.files?.data)">
-            <Icon name="ion:md-download" size="24" class="cursor-pointer hover:text-accent transition-[200ms]" />
+          <button
+            v-if="!loading"
+            aria-label="download-button"
+            class="res-download tooltip tooltip-accent z-20"
+            :data-tip="$t('material.download', filesCount)"
+            @click="download(item.files?.data)"
+          >
+            <Icon
+              name="ion:md-download"
+              size="24"
+              class="cursor-pointer hover:text-accent transition-[200ms]"
+            />
           </button>
         </div>
       </div>
