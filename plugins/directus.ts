5f77ce59addade8497d8030894ce0221a3ca5d73
HDDTHR <stevenjildeh@gmail.com>
2024-02-21 15:55:38 +0300

    Added the schema to the directus sdk

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,8 +1,9 @@
 import { createDirectus, rest, readItem, readItems } from '@directus/sdk';
+import { Schema } from 'types/schema';
 
 export default defineNuxtPlugin(() => {
 	const runtimeConfig = useRuntimeConfig()
-	const directus = createDirectus(runtimeConfig.public.apiUrl).with(rest());
+	const directus = createDirectus<Schema>(runtimeConfig.public.apiUrl).with(rest());
 
 	return {
 		provide: { directus, readItem, readItems },
