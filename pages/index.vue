7c1b54ed29d91ce38515c632226c2fa619bd02c2
HDDTHR <stevenjildeh@gmail.com>
2024-02-21 15:56:48 +0300

    Modify index page to fetch from directus sdk

--- pages/index.vue
+++ pages/index.vue
@@ -12,14 +12,14 @@
       }"
     >
       <NuxtLink
-        v-for="cat in categories?.data"
+        v-for="cat in categories"
         :key="cat.id"
         class="card max-h-24 shadow-xl border max-md:w-40 flex-1 grow w-36 min-w-[9rem]"
-        :class="`hover:bg-${cat.attributes.slug.toUpperCase()} border-${cat.attributes.slug.toUpperCase()}`"
-        :to="$nuxt.$localePath({ name: 'courses', query: { category: cat.attributes.slug.toLowerCase()}})"
+        :class="`hover:bg-${cat.slug.toUpperCase()} border-${cat.slug.toUpperCase()}`"
+        :to="$nuxt.$localePath({ name: 'courses', query: { category: cat.slug }})"
       >
         <div class="card-body p-4 font-bold justify-center items-center text-center">
-          {{ locale === "en" ? cat.attributes.name : cat.attributes.name_ar }}
+          {{ locale === "en" ? cat.name_en : cat.name_ar }}
         </div>
       </NuxtLink>
     </List>
@@ -31,10 +31,10 @@
     >
       <template v-if="favCourses">
         <CourseCard
-          v-for="item in favCourses?.data"
+          v-for="item in favCourses"
           :id="item.id"
           :key="item.id"
-          :item="item.attributes"
+          :item="item"
           class="border-base-content hover:bg-base-content hover:bg-opacity-10"
         />
       </template>
@@ -45,38 +45,30 @@
   </div>
 </template>
 <script setup lang="ts">
-  import qs from 'qs';
   const { locale } = useI18n();
+  const { $directus, $readItems } = useNuxtApp()
 
-  const { data: categories, pending } = useLazyAsyncData<
-    StrapiResponse<CategoryAttributes>
-  >(() => $baseApi("categories", { cache: true }));
+  const favList = ref<number[]>([])
+  
+  const { data: categories, pending } = useLazyAsyncData(() => $directus.request($readItems('category', {
+    sort: 'sort'
+  })))
 
-  // Get the list of favorite courses
-  const favList = ref<number[] | null>([])
-  const favQuery = reactive({
-    populate: ['resources'],
-    filters: computed(() => {
-      if (favList.value && favList.value.length > 0)
-        return {
-          id: {
-            $in: favList.value
+  const { data: favCourses, pending: favPending } = useLazyAsyncData(async () => {
+    if (!(favList.value.length == 0)){
+      return $directus.request($readItems('course', {
+        // @ts-ignore
+        fields: ['*', 'category.category_id.*'],
+        filter: {
+          'id': {
+            '_in': favList.value
           }
-        } as StrapiRestFilters<StrapiItem<CourseAttributes>>
-      else return null
-    })
-  })
-
-  const { data: favCourses, pending: favPending } =
-    await useLazyAsyncData(async (): Promise<StrapiResponse<CourseAttributes> | undefined> => {
-    if (favQuery.filters) {
-      // fetch favorite courses only if they exist
-      return await $baseApi(`courses?${qs.stringify(favQuery, { encodeValuesOnly: true })}`, { cache: true })
+        }
+      }))
     }
-    else return
   }, {
-    watch: [favQuery]
-  });
+    watch: [favList]
+  })
 
   onMounted(() => {
     const { getCourses } = useFavCourses()
