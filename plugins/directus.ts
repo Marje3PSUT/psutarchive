336ef01560cd1f4ca83bd6b4e3903b6579b35e42
HDDTHR <stevenjildeh@gmail.com>
2024-03-13 00:35:32 +0300

    Format files

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,11 +1,10 @@
-import { createDirectus, rest, readItem, readItems } from "@directus/sdk";
-import { Schema } from "types/schema";
+import { createDirectus, rest, readItem, readItems } from '@directus/sdk';
+import { Schema } from 'types/schema';
 
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig();
-  const directus = createDirectus<Schema>(runtimeConfig.public.apiUrl).with(
-    rest(),
-  );
+
+  const directus = createDirectus<Schema>(runtimeConfig.public.apiUrl).with(rest());
 
   return {
     provide: { directus, readItem, readItems },
--- server/api/version.get.ts
+++ server/api/version.get.ts
@@ -1,4 +1,4 @@
-import * as pkg from "@/package.json";
+import * as pkg from '@/package.json';
 
 export default defineEventHandler(() => {
   return pkg.version;
