2f7bdfd7c378a1e3ca9b79e5a97ec0d6fc454ce6
Bani <bbaltawalbeh23@cit.just.edu.jo>
2025-12-22 19:44:39 +0300

    resolve: local font ibm sans instead of google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,19 +8,6 @@ export default defineNuxtConfig({
           href: '/logo/logo_black.svg',
           type: 'image/svg+xml',
         },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com',
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com',
-          crossorigin: '',
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
-        },
       ],
       script: [
         {
